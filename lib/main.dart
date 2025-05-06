e7567d597899a9fd41df46b71f3261ab5921f05e
JehadISB <jib143327@gmail.com>
2025-05-06 11:14:16 +0300

    create app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tadwin_app/core/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: appTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
